d15689f71be007e345e3b562e82599958ef9903a
jcwilson11 <jwilson5@stevens.edu>
2023-02-17 01:05:05 -0500

    Hopefully fixed lab4

--- JW.CPE.390.Lab/Lab04/CPE390.Lab4Part2.1.s
+++ JW.CPE.390.Lab/Lab04/CPE390.Lab4Part2.1.s
@@ -7,8 +7,15 @@ _main:
 	ret
 
 	.globl f
+
+#set the value to be returned to be rax = 0x123456780E0CBA0
 f:
-	not	%rdx
-	and	%rcx, %rdx
-	mov	%rdx, %rax
-	ret
+ 	mov    %rcx, %rax     # Move rcx value to rax
+    mov    %rdx, %rbx     # Move rdx value to rbx
+    not    %rbx           # Invert rbx so that bits set to 1 in rdx are 0
+    and    %rax, %rbx     # Clear every bit in rax where rdx is 1
+    mov    %rbx, %rcx     # Move the modified value to rcx
+    mov    %rcx, %rax     # Move the modified value to rax
+    ret                   # Return the modified rcx value in rax             # Return the modified rcx value in rax
+
+	
